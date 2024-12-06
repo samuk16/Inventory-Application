d81e13e1e4a705922b58a5bf2124ef8771d27a6f
Samuk <marcelolamas19@gmail.com>
2024-12-06 16:53:13 -0300

    Add new queries.

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -4,3 +4,16 @@ export async function getAllManhwas() {
 	const { rows } = await pool.query("Select * from manhwas");
 	return rows;
 }
+
+export async function postAuthor(name: string) {
+	const { rows } = await pool.query(
+		"INSERT INTO authors (name_author) VALUES ($1)",
+		[name],
+	);
+	return rows;
+}
+
+export async function getAllAuthors() {
+	const { rows } = await pool.query("SELECT * FROM authors");
+	return rows;
+}
