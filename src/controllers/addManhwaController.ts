c43e8d8ae4e6f3dd680c9960fb1786c7e9d7f5fc
Samuk <marcelolamas19@gmail.com>
2024-12-06 16:55:27 -0300

    Remove unused files.

--- src/controllers/addManhwaController.ts
+++
@@ -1,5 +0,0 @@
-import type { Request, Response } from "express";
-
-export async function getAddManhwaGET(req: Request, res: Response) {
-	res.render("pages/addManhwa");
-}
--- src/routes/addManhwa.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getAddManhwaGET } from "../controllers/addManhwaController";
-const addManhwaRouter = Router();
-
-addManhwaRouter.get("/", getAddManhwaGET);
-
-export default addManhwaRouter;
