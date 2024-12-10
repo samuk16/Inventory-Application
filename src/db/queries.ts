f802d22871dae0a0e0dfae84fe950facf96e21bf
Samuk <marcelolamas19@gmail.com>
2024-12-10 16:09:48 -0300

    Add new queries.

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -17,3 +17,41 @@ export async function getAllAuthors() {
 	const { rows } = await pool.query("SELECT * FROM authors");
 	return rows;
 }
+export async function getAllTags() {
+	const { rows } = await pool.query("SELECT * FROM tags");
+	return rows;
+}
+export async function postManhwasTags(tags: number[], manhwaId: number) {
+	const tagsArr = tags.map((tag) => `(${tag},${manhwaId})`).join(",");
+	const query = `INSERT INTO manhwas_tags (tag_id,manhwa_id) VALUES ${tagsArr}`;
+
+	const { rows } = await pool.query(query);
+	return rows;
+}
+
+export async function verifyTitleManhwa(titleManhwa: string) {
+	const { rows } = await pool.query(
+		"SELECT title FROM manhwas WHERE title = $1",
+		[titleManhwa],
+	);
+	return rows;
+}
+export async function getManhwaId(titleManhwa: string) {
+	const { rows } = await pool.query("SELECT id FROM manhwas WHERE title = $1", [
+		titleManhwa,
+	]);
+	return rows;
+}
+export async function postManhwa(
+	title: string,
+	description: string,
+	caps: number,
+	url_img: string,
+	author_id: number,
+) {
+	const { rows } = await pool.query(
+		"INSERT INTO manhwas (title,description,caps,url_img,author_id) VALUES ($1,$2,$3,$4,$5)",
+		[title, description, caps, url_img, author_id],
+	);
+	return rows;
+}
