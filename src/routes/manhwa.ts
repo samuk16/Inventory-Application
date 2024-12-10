261967b4f5741160fdfd17e6683e3f556fcb723d
Samuk <marcelolamas19@gmail.com>
2024-12-10 16:15:42 -0300

    Add postManhwaC import to handle POST requests for manhwas.

--- src/routes/manhwa.ts
+++ src/routes/manhwa.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getAddManhwaGET, getManhwa } from "../controllers/manhwaController";
+import {
+	getAddManhwaGET,
+	getManhwa,
+	postManhwaC,
+} from "../controllers/manhwaController";
+
 const manhwaRouter = Router();
 
 manhwaRouter.get("/", getManhwa);
 manhwaRouter.get("/add", getAddManhwaGET);
-// manhwaRouter.post("/add-author", ...postAuthorF);
+manhwaRouter.post("/add", ...postManhwaC);
 
 export default manhwaRouter;
